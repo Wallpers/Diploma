3e26fe1a93eb0dad325f56c9ba28b7775058d4ec
Wallpers <Kokorin1506@mail.ru>
2017-04-29 10:34:09 +0300

    UI. Was developed aside menu. Css, js code is not refactored.

--- SmartMarket.UI/App_Start/BundleConfig.cs
+++ SmartMarket.UI/App_Start/BundleConfig.cs
@@ -9,10 +9,9 @@ namespace SmartMarket.UI
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/script.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
---
+++ SmartMarket.UI/Scripts/script.js
@@ -0,0 +1,26 @@
+﻿function openNav()
+{
+    var sidenav = $("#sidenav");
+
+    var top = $(".menu-top");
+    var middle = $(".menu-middle");
+    var bottom = $(".menu-bottom");
+
+
+    if (sidenav.hasClass("sidenav show"))
+    {
+        sidenav.removeClass("show");
+
+        top.removeClass("menu-top-click");
+        middle.removeClass("menu-middle-click");
+        bottom.removeClass("menu-bottom-click");
+    }
+    else
+    {
+        sidenav.addClass("show");
+
+        top.addClass("menu-top-click");
+        middle.addClass("menu-middle-click");
+        bottom.addClass("menu-bottom-click");
+    }
+}
