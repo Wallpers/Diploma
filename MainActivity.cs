5e6a27a403a7c6e4431badfafeb365fd46e50a8b
Wallpers <Kokorin1506@mail.ru>
2017-05-30 11:28:42 +0300

    Initial project. Login form.

---
+++ MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace SmartMarket.Android
+{
+    [Activity(MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        Button loginButton;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            loginButton = FindViewById<Button>(Resource.Id.loginButton);
+            loginButton.Click += LoginHandler;
+
+        }
+
+        private void LoginHandler(object sender, EventArgs e)
+        {
+            var email = FindViewById<EditText>(Resource.Id.emailEditText).Text;
+            var password = FindViewById<EditText>(Resource.Id.passwordEditText).Text;
+
+            throw new NotImplementedException();
+        }
+    }
+}
+
